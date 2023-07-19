ec41f138b14d00ce9d5b5aa835978bc369410d8e
Saurav Kumar <103248286+Saurav-K2002@users.noreply.github.com>
2023-07-19 19:11:13 +0530

    Update template.service.js

--- app/services/template.service.js
+++ app/services/template.service.js
@@ -1,6 +1,6 @@
-const from = '"Placement Portal" <placementportal@mnit.ac.in>';
-const baseUrl = 'http://placements.mnit.ac.in';
-const email_signature = '<br><br>With Regards.<br><br>Prof. Mahender Choudhary<br>In-charge, Training & Placement<br>MNIT Jaipur<br>+91-141-2529065';
+const from = '"Placement Portal" <placementportal@gift.edu.in>';
+const baseUrl = 'http://placements.gift.edu.in';
+const email_signature = '<br><br>With Regards.<br><br>Prof. Samir Panda<br>In-charge, Training & Placement<br>GIFT Bhubaneswar<br>+91-8292738623';
 
 exports.getEmailOpts = (data, mailType) => {
 
@@ -10,8 +10,8 @@ exports.getEmailOpts = (data, mailType) => {
             return {
                 from : from,
                 to:  data.college_email,
-                subject: 'Login Request : Placement Cell, MNIT Jaipur',
-                text: 'Hello '+ data.student_name + 'We have received a login request for your account.Please find the below OTP to proceed further With Regards, Prof. Mahendar Choudhary',
+                subject: 'Login Request : Placement Cell, GIFT Bhubaneswar',
+                text: 'Hello '+ data.student_name + 'We have received a login request for your account.Please find the below OTP to proceed further With Regards, Prof.Samir Panda',
                 html: 'Hello <strong>'+ data.student_name + '</strong>,<br><br>A sign in attempt to Placement Portal requires further verification to prevent unauthorized access to your account. To complete the sign in, enter the verification code on the Placement Portal.<br><br>Verification Code: ' + data.login_otp + email_signature
             }
             break;
@@ -20,8 +20,8 @@ exports.getEmailOpts = (data, mailType) => {
             return {
                 from: from,
                 to: data.college_email,
-                subject: 'Reset Password Request : Placement Cell, MNIT Jaipur',
-                text: 'Hello '+ data.student_name + 'You requested for the reset password.Please find the below link Reset password With Regards, Prof. Mahendar Choudhary',
+                subject: 'Reset Password Request : Placement Cell, GIFT Bhubaneswar',
+                text: 'Hello '+ data.student_name + 'You requested for the reset password.Please find the below link Reset password With Regards, Prof. Samir Panda',
                 html: 'Hello <strong>'+ data.student_name + '</strong>,<br><br>You requested for the reset password. Please find the below link<br><br><a href="' + baseUrl + "/forgotPassword/" + data.temporarytoken + '">Reset password</a>'+ email_signature
             }
             break;
@@ -30,8 +30,8 @@ exports.getEmailOpts = (data, mailType) => {
             return {
                 from: from,
                 to: data.college_email,
-                subject: 'Password Updated : Placement Cell, MNIT Jaipur',
-                text: 'Hello '+ data.student_name + 'Your password has been successfully updated.With Regards, Prof. Mahendar Choudhary',
+                subject: 'Password Updated : Placement Cell, GIFT Bhubaneswar',
+                text: 'Hello '+ data.student_name + 'Your password has been successfully updated.With Regards, Prof. Samir Panda',
                 html: 'Hello <strong>'+ data.student_name + '</strong>,<br><br>Your password has been successfully updated.'+ email_signature
             }
             break;
@@ -41,7 +41,7 @@ exports.getEmailOpts = (data, mailType) => {
                 from: from,
                 to: data.author_id + '@mnit.ac.in',
                 subject: 'Yay! We have published your article ' + data.title,
-                text: 'Hello '+ data.author_name + 'Thanks for sharing your interview process and thoughts with us With Regards, Prof. Mahendar Choudhary',
+                text: 'Hello '+ data.author_name + 'Thanks for sharing your interview process and thoughts with us With Regards, Prof. Samir Panda',
                 html: 'Hello <strong>'+ data.author_name + '</strong>,<br><br>Thanks for sharing your interview process and thoughts with us to help others. We have published your interview experience after a few modifications. We wish you luck for the future! Please find the link below -<br><br><a href="' + baseUrl + "/experience/" + data._id + '">' + data.title + ' </a>'+ email_signature
             };
             break;
